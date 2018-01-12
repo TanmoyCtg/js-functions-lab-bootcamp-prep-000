3c8a529307454285f475e451c475a1d2940b0c02
iftekhar joy <iftekharjoy1@gmail.com>
2018-01-12 17:05:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(string name) {
+function happyHolidaysTo( name) {
   return "Happy holidays, ${name}!"
 }
 function happyHolidayTo(holiday,name) {
